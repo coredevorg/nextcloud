0b88b732934e64b07a0ba52b2edb20c43ab2aece
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-26 18:03:12 +0100

    generate MYSQL_ROOT_PASSWORD

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 [ "$1" ] || { echo "usage: ./setup.sh <FQDN>" 1>&2 ; exit 1;  }
 [ -f ".env" ] || cp env.setup .env
+MYSQL_ROOT_PASSWORD=$(pwgen 16 1)
 MYSQL_PASSWORD=$(pwgen 16 1)
-sed -i -e "s/{{VIRTUAL_HOST}}/$1/g" -e "s/{{MYSQL_PASSWORD}}/${MYSQL_PASSWORD}/g" .env
+sed -i -e "s/{{VIRTUAL_HOST}}/$1/g" -e "s/{{MYSQL_PASSWORD}}/${MYSQL_ROOT_PASSWORD}/g" -e "s/{{MYSQL_PASSWORD}}/${MYSQL_PASSWORD}/g" .env

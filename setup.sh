8f9e3fc2780a1939c35c7e666bf8fd9ecd23bc55
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-28 03:50:47 +0200

    optimized startup.sh script

--- setup.sh
+++ setup.sh
@@ -3,4 +3,10 @@
 [ -f ".env" ] || cp env.setup .env
 MYSQL_ROOT_PASSWORD=$(pwgen 16 1)
 MYSQL_PASSWORD=$(pwgen 16 1)
-sed -i -e "s/{{VIRTUAL_HOST}}/$1/g" -e "s/{{MYSQL_ROOT_PASSWORD}}/${MYSQL_ROOT_PASSWORD}/g" -e "s/{{MYSQL_PASSWORD}}/${MYSQL_PASSWORD}/g" .env
+sed -i  -e "s/{{VIRTUAL_HOST}}/$1/g" \
+        -e "s/{{MYSQL_ROOT_PASSWORD}}/${MYSQL_ROOT_PASSWORD}/g" \
+        -e "s/{{MYSQL_PASSWORD}}/${MYSQL_PASSWORD}/g" .env
+echo -n "continue with startup? " && read dummy
+docker-compose -f docker-compose.yml up -d --build
+echo -n "show startup logs? " && read dummy
+docker-compose -f docker-compose.yml logs -f --tail 1000
